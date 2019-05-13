74e4f8b672a2400c20118127b8709fe7477643e5
Miyamizu Mitsuha <41120381+etylix@users.noreply.github.com>
2019-05-13 16:21:23 +0700

    sirius: AOSP bring up

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -6,11 +6,12 @@
 
 $(call inherit-product, device/xiaomi/sirius/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cosp/config/common_full_phone.mk)
-
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cosp_sirius
+PRODUCT_NAME := aosp_sirius
 PRODUCT_DEVICE := sirius
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
