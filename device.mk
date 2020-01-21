0578a5af1b8fa57b40178b8f5a03eb2e9477f5a2
Alex <alexeydomoratsky1@gmail.com>
2020-01-21 19:07:39 +0300

    sirius: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml
