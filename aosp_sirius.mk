8b6d164342c369d9112f03a25624a195fdf73b3f
solidv <pedroclaro2@gmail.com>
2020-02-02 16:08:47 +0000

    sirius: Use pixel fp to bypass safetynet

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -21,7 +21,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/sirius/sirius8.1.0/OPM1.171019.019/V9.5.6.0.OEBCNFA:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 8.1.0 OPM1.171019.019 V9.5.6.0.OEBCNFA release-keys" \
@@ -29,3 +29,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="sirius"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
