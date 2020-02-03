f0bbd3ae180d56400e2d435a045b8de802b71acb
solidv <pedroclaro2@gmail.com>
2020-02-03 13:53:06 +0000

    sirius: add missing display props

--- device-props.mk
+++ device-props.mk
@@ -12,7 +12,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.has_HDR_display=true
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
