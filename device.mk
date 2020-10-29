4ebcd5e763ba1be251b2734590234710e70e7840
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    sirius: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -107,6 +107,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # WiFi
 PRODUCT_PACKAGES += \
     TargetWifiOverlay
