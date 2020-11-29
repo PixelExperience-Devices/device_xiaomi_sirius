6ffccabd5e1b1045c1518f457ef139aa2ffd30f7
solidv <pedroclaro2@gmail.com>
2020-11-29 13:48:01 +0000

    sdm710-common: Drop deprecated flags

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -8,9 +8,6 @@ $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
